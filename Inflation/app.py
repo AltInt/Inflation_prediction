f760a59ba9775d89312d9fec1f46ca63850117a3
Amir El Aouadi <a.elaouadi@hotmail.com>
2022-09-14 16:45:12 +0100

    latest notebook

--- Inflation/app.py
+++ Inflation/app.py
@@ -34,7 +34,7 @@ if st.button('Predict!'):
 
 def get_line_chart_data():
 
-    return pd.read_csv('../data/final_df.csv')
+    return pd.read_csv('data/final_df.csv')
 
 
 
@@ -43,7 +43,7 @@ df = get_line_chart_data().set_index('Date')
 y = df['RPI']
 # y1 = df['RPI_YOY']
 y_test5 = y[163:164]
-model6 = load_model('../data/final_prediction_graph')
+model6 = load_model('data/final_prediction_graph')
 test_results5 = prediction(model6)
 test_results5.index = pd.to_datetime(test_results5.index)
 
@@ -56,10 +56,11 @@ y.index = pd.to_datetime(y.index)
 def plot_pred(y, y_test5, test_results5):
     fig, ax = plt.subplots()
     ax.plot(y, label = 'RPI')
-    ax.scatter([test_results5.index], test_results5['test_predictions'], label='prediction')
-    ax.set_title('Inflation Prediciton')
+    ax.scatter([test_results5.index], test_results5['test_predictions'], label='prediction', color='r')
+    ax.set_title('Inflation Prediction')
     ax.set_xlabel('Years')
     ax.set_ylabel('RPI')
+    ax.set_ylim(200,360)
     ax.legend()
     fig.show()
 
