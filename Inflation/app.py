c2886019d9a9efc3894ad38f4fbb2217d1249caf
Amir <109305387+amirlewagon@users.noreply.github.com>
2022-09-15 15:16:21 +0100

    Update app.py

--- Inflation/app.py
+++ Inflation/app.py
@@ -1,3 +1,5 @@
+from multiprocessing import parent_process
+from pyexpat import model
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -59,20 +61,16 @@ def predict(model_selection):
         prediction_value_1 = (prediction1.iloc[0]/10)*307.4
         prediction_value_2 = (prediction2.iloc[0]/10)*307.4
         prediction = (prediction_value_1+prediction_value_2)/2
+        st.write(prediction)
         results = pd.DataFrame(columns = ['test_predictions', 'test_actual'], index = y_test5.index)
-        results['test_predictions'] = prediction[163]
+        results['test_predictions'] = prediction
         results['test_actual'] = 345.2
 
     else:
-        pass
+        st.write('No model was chosen. Please refresh the page and try again.')
+        return None
 
     return results
-
-##TO DO: FORMAT OUTPUT, round pred
-##Add truth value
-##Add chart comparing both
-
-
 
 def plotly_lstm():
     df = get_line_chart_data().set_index('Date')
@@ -96,6 +94,31 @@ def plotly_sarimax():
     fig_sar = go.Figure(data=fig1.data + fig2.data)
     return fig_sar
 
+
+def plotly_arimax():
+    df = get_line_chart_data().set_index('Date')
+    model_ar1 = load_model_arima1()
+    model_ar2 = load_model_arima2()
+    arratst = np.array([[343.54180382,  11.04269294, -10.64054131,  58.34667398]])
+    y = df['RPI']
+    prediction1 = model_ar1.predict(start=61,end=61,exog = arratst,dynamic=True)
+    prediction2 = model_ar2.predict(start=61,end=61,exog = arratst,dynamic=True)
+    prediction_value_1 = ((prediction1.iloc[0]*0.01)+1)*307.4
+    prediction_value_2 = ((prediction2.iloc[0]*0.01)+1)*307.4
+    prediction = (prediction_value_1+prediction_value_2)/2
+
+    test_result = pd.DataFrame(columns=['test_predictions', 'test_actual'])
+    test_result['test_predictions'] = prediction
+    test_result['test_actual'] = 345.2
+    test_result['date'] = y.index[163]
+
+    fig1 = px.line(y)
+    fig2 = px.scatter(test_result["test_predictions"], color_discrete_sequence=['red'])
+    fig_ar = go.Figure(data=fig1.data + fig2.data)
+    return fig_ar
+
+
+
 if st.button('Predict!'):
     prediction = predict(model_selection)
     st.markdown('## Prediction made!📈')
@@ -109,38 +132,6 @@ if st.button('Predict!'):
         fig_sar = plotly_sarimax()
         st.plotly_chart(fig_sar)
 
-
-
-
-
-
-
-
-
-    st.pyplot(plotly_lstm())
-# st.plotly_chart(plotly_lstm())
-
-
-
-# #MAKE ACTUAL_DATA = TODAY'S RPI PRINT
-
-# #st.line_chart(df[['RPI','CPI']])
-
-# y.index = pd.to_datetime(y.index)
-
-# def plot_pred(y, y_test5, test_results5):
-#     fig, ax = plt.subplots()
-#     ax.plot(y, label = 'RPI')
-#     ax.scatter([test_results5.index], test_results5['test_predictions'], label='prediction')
-#     ax.set_title('Inflation Prediciton')
-#     ax.set_xlabel('Years')
-#     ax.set_ylabel('RPI')
-#     ax.legend()
-#     fig.show()
-
-#     return fig
-
-# ADD TEXT YOUR TURN
-
-# fig = plot_pred(y, y_test5, test_results5)
-# st.pyplot(fig)
+    elif model_selection == 'ARIMAX':
+        fig_ar = plotly_arimax()
+        st.plotly_chart(fig_ar)
